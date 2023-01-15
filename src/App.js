82619e7499791a74b1e2e4337b55bdcaeaafa91c
mosesochacha <ochachamoses443@gmail.com>
2023-01-15 13:07:26 +0300

    complited fetch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DataFetching from './DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DataFetching.js
@@ -0,0 +1,62 @@
+import { useState  ,useEffect} from 'react';
+import axios from 'axios';
+const API = " http://localhost:3000/transactions"
+
+ export default function DataFetching(){
+    const [transactions , setTransanction] = useState([])
+
+    useEffect(()=>{
+        axios.get(API)
+        .then((resp)=>{
+            console.log(resp);
+            setTransanction(resp.data)
+        })
+    },[])
+    return(
+        <div>
+            
+            <table>
+              <thead>
+                <tr>
+                <th>DATE</th>
+                <th>DESCRIPTION</th>
+                <th>CATEGORY</th>
+                <th>AMOUNT</th> 
+                </tr>
+              </thead>
+              <tbody>
+              {
+                 transactions.map(dataObj=>
+                        <tr key={dataObj.id}>
+                            <td>{dataObj.date}</td>
+                            <td>{dataObj.description}</td>
+                            <td>{dataObj.category}</td>
+                            <td>{dataObj.amount}</td>
+                        </tr>
+                    
+                    )
+                }
+              </tbody>
+
+            </table>
+            {/* <table>
+                
+                <th>DATE</th>
+                <th>DESCRIPTION</th>
+                <th>CATEGORY</th>
+                <th>AMOUNT</th>
+                
+                {
+                 transactions.map(dataObj=>
+                    <td key={dataObj.id}>
+                        <tr>{dataObj.date}</tr>
+                        <tr>{dataObj.description}</tr>
+                        <tr>{dataObj.category}</tr>
+                        <tr>{dataObj.amount}</tr>
+                    </td>
+                    )
+                }
+            </table> */}
+        </div>
+    )
+}
